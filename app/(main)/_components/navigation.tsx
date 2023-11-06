3a499c60a906a4ea343b5bfd9645b8ce25bd8d3b
LarinNikita <larin_nikita_m@mail.ru>
2023-11-06 16:10:12 +0700

    Reuseble sidebar item

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,21 +1,24 @@
 "use client";
 
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 
 import { UserItem } from "./user-item";
+import { Item } from "./item";
 
-import { useQuery } from "convex/react";
+import { useQuery, useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
 
     const documents = useQuery(api.documents.get);
+    const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -23,6 +26,7 @@ export const Navigation = () => {
     const [isResetting, setIsResetting] = useState(false);
     const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
+    //#region resizing
     useEffect(() => {
         if (isMobile) {
             collapse();
@@ -91,6 +95,17 @@ export const Navigation = () => {
             setTimeout(() => setIsResetting(false), 300);
         }
     };
+    //#endregion
+
+    const handleCreate = () => {
+        const promise = create({ title: "Untitled" });
+
+        toast.promise(promise, {
+            loading: "Create a new note...",
+            success: "New note created!",
+            error: "Failed to create a new note"
+        });
+    };
 
     return (
         <>
@@ -114,6 +129,22 @@ export const Navigation = () => {
                 </div>
                 <div>
                     <UserItem />
+                    <Item
+                        label="Search"
+                        icon={Search}
+                        isSearch
+                        onClick={() => { }}
+                    />
+                    <Item
+                        label="Settings"
+                        icon={Settings}
+                        onClick={() => { }}
+                    />
+                    <Item
+                        onClick={handleCreate}
+                        label="New page"
+                        icon={PlusCircle}
+                    />
                 </div>
                 <div className="mt-4">
                     {documents?.map((document) => (
